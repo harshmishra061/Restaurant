b1ab4400c3a50f050e0d896889fefe11de79c237
Harsh Mishra <harshmishra27122003@gmail.com>
2025-03-16 16:29:36 +0530

    Add login logics in frontend

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,15 +1,34 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import { Grid2 as Grid, Link, Typography } from '@mui/material'
+import { useNavigate } from 'react-router'
+import axios from 'axios'
+import { AuthContext } from '../../contexts/authContext'
 const Login = () => {
+    const API_URL = import.meta.env.VITE_BACKEND_URL;
     const [data, setData] = useState({
         email: '',
         password: '',
     })
+    const navigate = useNavigate()
+    const [error, setError] = useState('')
+    const {addToken} = useContext(AuthContext)
 
-    const loginUser = () => {
-        console.log(data)
+    const loginUser = async () => {
+        console.log(data);
+        try {
+            const url = `${API_URL}login`;
+            const response = await axios.post(
+                url,
+                data
+            )
+            let token = 'abcdef'
+            addToken(token)
+            navigate('/')
+        } catch (error) {
+            setError(error.response.data)
+        }
     }
     return (
         <Grid
@@ -43,6 +62,10 @@ const Login = () => {
             <Typography variant="body1" textAlign={'center'}>
                 Create an account ? <Link href="/signup">Signup</Link>
             </Typography>
+            <Typography variant="body1" color={'error'} textAlign={'center'}>
+                {error}
+            </Typography>
+
         </Grid>
     )
 }
